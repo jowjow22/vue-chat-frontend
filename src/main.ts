96c91f59c51684505054c3cfe04d41c4fa53eec1
jowjow22 <jonathan224santos@gmail.com>
2022-11-30 11:01:01 -0300

    feat: add url to cors validation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import store from "./store";
+import SocketIO from "socket.io-client";
 import VueSocketIO from "vue-3-socket.io";
+
+const socketConnection = SocketIO("https://vue-chat-backend.vercel.app/");
 
 const socket = new VueSocketIO({
   debug: true,
-  connection: "https://vue-chat-backend.vercel.app/",
+  connection: socketConnection,
   vuex: {
     store,
     actionPrefix: "SOCKET_",
